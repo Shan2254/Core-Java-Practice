311aae67390d29d90a55d2011db6ca6f1e971168
Shantanu Joshi <107461071+Shan2254@users.noreply.github.com>
2023-06-21 00:07:52 +0530

    Update ReverseString.java

--- src/com/string/ReverseString.java
+++ src/com/string/ReverseString.java
@@ -2,6 +2,15 @@ package com.string;
 
 public class ReverseString {
     public static void main(String[] args) {
-        
+        System.out.println("Enter a string you want to reverse: ");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        char ch[] = s.toCharArray();
+        String rev = "";
+
+        for(int i=ch.length-1; i>=0; i--) {
+            rev = rev + ch[i];
+        }
+        System.out.println("Reverse of given string is: "+rev);
     }
 }
